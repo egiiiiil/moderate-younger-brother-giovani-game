c808172869b6b083ea6de1df6d5b7d26884d30c6
K1-RA <indrajeetgodse@gmail.com>
2021-11-18 10:13:53 +0100

    SetUp
    have matched the code to josef's with slight modifications for our base

---
+++ scripts.js
@@ -0,0 +1,16 @@
+import Giovanni from "./Giovanni.js";
+
+var config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 500,
+	scene: [Giovanni],
+	physics: {
+		default: "arcade",
+		arcade: {
+			gravity: false,
+		},
+	},
+};
+
+new Phaser.Game(config);
