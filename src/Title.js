f6938ba93aae90894d61d3d5d62693e5955b97e8
K1-RA <indrajeetgodse@gmail.com>
2021-11-25 14:32:26 +0100

    working on highscores and added new sprites

--- src/Title.js
+++ src/Title.js
@@ -1,20 +1,19 @@
-import Phaser from "phaser"
-import Giovanni from "./Giovanni.js"
+import Phaser from "phaser";
+import Giovanni from "./Giovanni.js";
+import Highscore from "./HighscorePage.js";
 
 export default class TitleScene extends Phaser.Scene {
 	constructor() {
-		super({key: "TitleScene"})
+		super({ key: "TitleScene" });
 	}
-	preload() {
-
-	}
+	preload() {}
 	create() {
-		this.add.text(5, 5, "Moderate Younger Brother Giovani Game")
-		this.add.text(5, 20, "Press any key to start")
-		this.input.keyboard.on('keydown', () => {
-			this.scene.add("Giovanni", Giovanni, true)
-			this.scene.remove("TitleScene")
-
-		})
+		this.add.text(5, 5, "Moderate Younger Brother Giovani Game");
+		this.add.text(5, 20, "Press any key to start");
+		this.input.keyboard.on("keydown", () => {
+			// this.scene.add("Giovanni", Giovanni, true);
+			this.scene.add("Highscore", Highscore, true);
+			this.scene.remove("TitleScene");
+		});
 	}
 }
